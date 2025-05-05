b9a15b98b47a52b03b52dff2ae20a36f62aa20d3
Syed <muzasyed@amd.com>
2025-05-05 16:29:33 -0400

    adding more scripts
    Signed-off-by: Syed <muzasyed@amd.com>

---
+++ scripts/verify/verifyNumerics.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+source ~/.bashrc
+
+####
+# Usage
+# !IMPORTANT: Make sure your project is set up with undo.sh and setup.sh scripts
+# [1] - which project to verify numerics for
+# [2] - Where the testbench is
+# [3] - What the pattern to match against is (default "tb.mlir")
+####
+
+
+
+if [ "$3" == " " ]; then
+  pattern="$SRC_DIR/*_tb.mlir"
+else
+  pattern="$SRC_DIR/$3"
+fi
+
+SRC_DIR=$2
+
+# rebuildc
+setLocal $1
+
+for filepath in $pattern; do
+  if [ -f "$filepath" ]; then
+    file=${filepath##*/}
+    echo $filepath
+    [[ $file =~ ([0-9]*)_([0-9]*)_([0-9]*)_.*$ ]]
+    args=("${BASH_REMATCH[@]}")
+    args=("${args[@]:1}")
+    echo ${args[@]}
+    firstDim=("${args[0]}")
+    secondDim=("${args[2]}")
+    thirdDim=("${args[1]}")
+    maskNpy tensor_${firstDim}_${thirdDim} $firstDim $thirdDim
+    # arangeNpy tensor_${firstDim}_${thirdDim} $firstDim $thirdDim
+    maskNpy mask_${secondDim}_${thirdDim} $secondDim $thirdDim
+  fi
+done
+
+ITER="main"
+echo "Setting up ${ITER}"
+bash /home/muzasyed/projects/project2/undo.sh
+# cp $LOCAL/subgroup2.cpp /home/muzasyed/iree/third_party/llvm-project/mlir/lib/Dialect/GPU/Transforms/SubgroupReduceLowering.cpp
+
+for filepath in $pattern; do
+  if [ -f "$filepath" ]; then
+    file=${filepath##*/}
+    # [[ $file =~ gemm_([0-9]*)_([0-9]*)_([0-9]*)_.*$ ]]
+    [[ $file =~ ([0-9]*)_([0-9]*)_([0-9]*)_.*$ ]]
+    args=("${BASH_REMATCH[@]}")
+    args=("${args[@]:1}")
+    firstDim=("${args[0]}")
+    secondDim=("${args[2]}")
+    thirdDim=("${args[1]}")
+    echo "Compiling ${ITER}"
+    iree-compile $filepath -o $SRC_DIR/$file.${ITER}.vmfb --iree-hal-target-device=hip --iree-hip-target=gfx942 --mlir-print-ir-after-all --debug-only=iree-llvmgpu-kernel-config &> $SRC_DIR/$file.${ITER}.mlir
+    echo "Running ${ITER}"
+    iree-run-module --device=hip --module=$SRC_DIR/$file.${ITER}.vmfb --input=@$GENERATE/tensor_${firstDim}_${thirdDim}.npy --input=@$GENERATE/mask_${secondDim}_${thirdDim}.npy --output=@$SRC_DIR/output_${firstDim}_${secondDim}.${ITER}.npy &> $SRC_DIR/$file.${ITER}.result
+    echo "$SRC_DIR/$file.${ITER}.result"
+  fi
+done
+
+ITER="mine"
+echo "Setting up ${ITER}"
+bash /home/muzasyed/projects/project2/setup.sh
+# cp $LOCAL/subgroup.cpp /home/muzasyed/iree/third_party/llvm-project/mlir/lib/Dialect/GPU/Transforms/SubgroupReduceLowering.cpp
+
+for filepath in $SRC_DIR/*_f16*_tB.mlir; do
+  if [ -f "$filepath" ]; then
+    file=${filepath##*/}
+    [[ $file =~ ([0-9]*)_([0-9]*)_([0-9]*)_.*$ ]]
+    args=("${BASH_REMATCH[@]}")
+    args=("${args[@]:1}")
+    firstDim=("${args[0]}")
+    secondDim=("${args[2]}")
+    thirdDim=("${args[1]}")
+    echo "Compiling ${ITER}"
+    iree-compile $filepath -o $SRC_DIR/$file.${ITER}.vmfb --iree-hal-target-device=hip --iree-hip-target=gfx942 --mlir-print-ir-after-all --debug-only=iree-llvmgpu-kernel-config &> $SRC_DIR/$file.${ITER}.mlir
+    echo "Running ${ITER}"
+    iree-run-module --device=hip --module=$SRC_DIR/$file.${ITER}.vmfb --input=@$GENERATE/tensor_${firstDim}_${thirdDim}.npy --input=@$GENERATE/mask_${secondDim}_${thirdDim}.npy --output=@$SRC_DIR/output_${firstDim}_${secondDim}.${ITER}.npy &> $SRC_DIR/$file.${ITER}.result
+    echo "$SRC_DIR/$file.${ITER}.result"
+  fi
+done
+
+for filepath in $SRC_DIR/*_f16*_tB.mlir; do
+  if [ -f "$filepath" ]; then
+    file=${filepath##*/}
+    # [[ $file =~ gemm_([0-9]*)_([0-9]*)_([0-9]*)_.*$ ]]
+    [[ $file =~ ([0-9]*)_([0-9]*)_([0-9]*)_.*$ ]]
+    args=("${BASH_REMATCH[@]}")
+    args=("${args[@]:1}")
+    firstDim=("${args[0]}")
+    secondDim=("${args[2]}")
+    thirdDim=("${args[1]}")
+    checkDiff $SRC_DIR/output_${firstDim}_${secondDim}.main.npy $SRC_DIR/output_${firstDim}_${secondDim}.mine.npy &> $SRC_DIR/diffs_${firstDim}_${secondDim}.txt
+  fi
+done
+
+# echo "$shape"
+# iree-compile ${ITER} -o $SRC_DIR --iree-hal-target-device=hip --iree-hip-target=gfx942 --mlir-print-ir-after-all
+# $IN_MLIR/$foo.mlir $IN_VMFB/$foo.main.vmfb &> $OUTPUT/$foo.main.mlir
+
+# iree-run-module --device=hip --module=$IN_VMFB/$foo.mine.vmfb -
+# -input=1x64x64xf8E4M3FNUZ=$tensor1 --input=1x64x64xf8E4M3FNUZ=$tensor2 --input=1x64x64xf8E4M3FNUZ=$tensor3 
+# &> $OUTPUT/$foo.mine.log
